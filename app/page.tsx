9313fb35ea0debc954a54f59f11d28a62cfde8fd
Snekith Karthikeyan <136680118+snekith99@users.noreply.github.com>
2024-01-30 01:10:35 +1100

    Added nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import IntroSection from "./components/IntroSection";
+import HeaderSection from "../components/HeaderSection";
+import IntroSection from "../components/IntroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col bg-[#121212] items-center p-36">
+      <HeaderSection/>
       <IntroSection/>
     </main>
   );
